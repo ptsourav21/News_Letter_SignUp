b5e6770106b198102db2210145c4dc255699d59a
Purnendu Talukder <ptsourav21@gmail.com>
2023-09-09 20:47:30 +0600

    update

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function (req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  const list = "c9733bd4c9";
+  const url = "https://us12.api.mailchimp.com/3.0/lists/" + list;
+  options = {
+    method: "POST",
+    auth: "ptsourav21:93764e46af507db4d75d553c789f31c6-us12",
+  };
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+app.listen(3000, function () {
+  console.log("Server is running");
+});
+// 93764e46af507db4d75d553c789f31c6-us12
+// c9733bd4c9
